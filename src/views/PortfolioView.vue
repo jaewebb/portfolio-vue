f8aa55a35b4a132a9908fb8bf5a3bb968910e972
Jae Webb <jynnae.webb@gmail.com>
2024-05-22 23:07:56 -0400

    added github link

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -6,6 +6,8 @@
         Most of what I've worked on has been used internally,
         so I'm starting a portfolio page with proof of concepts!
       </p>
+      <h2 class="h4 text-lime-500">Github</h2>
+      <p>Visit my <a href="https://github.com/jaewebb" class="underline">GitHub</a> page to check out my code</p>
       <h2 class="h4 text-lime-500">Works in Progress:</h2>
       <ul>
         <li>
